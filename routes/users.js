a7d5e3ed8c5e877cadca0540ce72c825babb3e29
manumontes23 <manumontes23@gmail.com>
2019-10-01 19:55:22 -0500

    lol

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,13 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
 
 module.exports = router;
